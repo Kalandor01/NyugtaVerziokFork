2ced2275373019ad680dd99cb5345821552b77bb
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-17 13:51:29 +0100

    final

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -6,11 +6,12 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 */
 public class Nyugta3
 {
-    public static void kiir_line(String jel) {
-        for (int x = 0; x < 20; x++) {
+    public static void kiir_line(String jel, int len) {
+        for (int x = 0; x < len; x++) {
             System.out.print(jel);
         }
-        System.out.println();
+        if(len==20)
+            System.out.println();
     }
 
     
@@ -25,14 +26,17 @@ public class Nyugta3
     
     public static void keltezes()
     {
-        final String rovidVonal = "_______";
-        final String rovidVonalValaszto = "      ";
         System.out.println();
-        System.out.println(rovidVonal + rovidVonalValaszto + rovidVonal);
-        System.out.println(" Dátum" + rovidVonalValaszto + "   Név");
-        kiir_line("*");
+        kiir_line("_", 7);
+        kiir_line(" ", 6);
+        kiir_line("_", 7);
+        System.out.println();
+        System.out.print(" Dátum");
+        kiir_line(" ", 9);
+        System.out.println("Név");
+        kiir_line("*", 20);
         System.out.printf("%11s\n", "CÉG");
-        kiir_line("*");
+        kiir_line("*", 20);
     }
 
     public static void main(String[] args) {
@@ -42,29 +46,29 @@ public class Nyugta3
         int szervizDijMertek = 10;
         int[] tetelek = new int[] {350, 90, 1320};
         //header
-        kiir_line("*");
+        kiir_line("*", 20);
         System.out.printf("%14s\n", "Nyugta 3");
-        kiir_line("*");
+        kiir_line("*", 20);
         tetelek_kiir(tetelek, HUF);
         //összesen
-        kiir_line("=");
+        kiir_line("=", 20);
         int osszesen = 0;
         for (int tetel : tetelek) {
             osszesen += tetel;
         }
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
-        kiir_line("-");
+        kiir_line("-", 20);
         //szervízdíj
         int szervizDij = osszesen / szervizDijMertek;
         System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
-        kiir_line("=");
+        kiir_line("=", 20);
         //fizetendő
         int fizetendo = osszesen + szervizDij;
         System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
         double euro = fizetendo / 350.0;
         System.out.printf("%10s%7.2f %s\n","", euro, eur);
-        kiir_line("-");
+        kiir_line("-", 20);
         keltezes();
     }
 
